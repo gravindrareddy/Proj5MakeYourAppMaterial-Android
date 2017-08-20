6ddcadd7916324e95afdf2628c43ccf976cd58ea
gravindrareddy <raviguntaka.reddy@gmail.com>
2017-08-21 00:07:53 +0530

    + Detail view with CoordinatorLayout, CollapseToolbar, etc is ready with dummy data + ToDo: Fill the detail view with the original data + ToDo: Apply SharedTransition (ref: http://engineering.letsnurture.com/transitions-with-shared-elementsmaterial-design/) + ToDo: Fix Detail layout to support Tablet

--- XYZReader/src/main/java/redgun/example/materialdesign/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/redgun/example/materialdesign/ui/ArticleDetailFragment.java
@@ -1,18 +1,25 @@
 package redgun.example.materialdesign.ui;
 
-import android.app.Fragment;
-import android.app.LoaderManager;
+
 import android.content.Intent;
-import android.content.Loader;
+
 import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.format.DateUtils;
 import android.text.method.LinkMovementMethod;
@@ -22,38 +29,38 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
+
 import redgun.example.materialdesign.R;
 import redgun.example.materialdesign.data.ArticleLoader;
+
 
 /**
  * A fragment representing a single Article detail screen. This fragment is
  * either contained in a {@link ArticleListActivity} in two-pane mode (on
  * tablets) or a {@link ArticleDetailActivity} on handsets.
  */
+
 public class ArticleDetailFragment extends Fragment implements
         LoaderManager.LoaderCallbacks<Cursor> {
     private static final String TAG = "ArticleDetailFragment";
 
     public static final String ARG_ITEM_ID = "item_id";
-    private static final float PARALLAX_FACTOR = 1.25f;
-
     private Cursor mCursor;
     private long mItemId;
     private View mRootView;
-    private int mMutedColor = 0xFF333333;
-    private ObservableScrollView mScrollView;
-    private DrawInsetsFrameLayout mDrawInsetsFrameLayout;
-    private ColorDrawable mStatusBarColorDrawable;
-
-    private int mTopInset;
-    private View mPhotoContainerView;
-    private ImageView mPhotoView;
-    private int mScrollY;
     private boolean mIsCard = false;
     private int mStatusBarFullOpacityBottom;
+    CoordinatorLayout activity_detail_coordinatorlayout;
+    AppBarLayout article_detail_appbarlayout;
+    CollapsingToolbarLayout article_detail_collapsetoolbarlayout;
+    ImageView article_detail_image;
+    TextView article_detail_title, article_byline, article_detail_description;
+    Toolbar article_detail_toolbar;
+    FloatingActionButton article_detail_share_fab;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -74,18 +81,11 @@ public class ArticleDetailFragment extends Fragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments().containsKey(ARG_ITEM_ID)) {
-            mItemId = getArguments().getLong(ARG_ITEM_ID);
+        if (getArguments().containsKey("selected_article_id")) {
+            mItemId = getArguments().getInt("selected_article_id");
         }
 
-        mIsCard = getResources().getBoolean(R.bool.detail_is_card);
-        mStatusBarFullOpacityBottom = getResources().getDimensionPixelSize(
-                R.dimen.detail_card_top_margin);
-        setHasOptionsMenu(true);
-    }
 
-    public ArticleDetailActivity getActivityCast() {
-        return (ArticleDetailActivity) getActivity();
     }
 
     @Override
@@ -96,39 +96,45 @@ public class ArticleDetailFragment extends Fragment implements
         // the fragment's onCreate may cause the same LoaderManager to be dealt to multiple
         // fragments because their mIndex is -1 (haven't been added to the activity yet). Thus,
         // we do this in onActivityCreated.
-        getLoaderManager().initLoader(0, null, this);
+        //getLoaderManager().initLoader(0, null, this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_article_detail, container, false);
-        mDrawInsetsFrameLayout = (DrawInsetsFrameLayout)
-                mRootView.findViewById(R.id.draw_insets_frame_layout);
-        mDrawInsetsFrameLayout.setOnInsetsCallback(new DrawInsetsFrameLayout.OnInsetsCallback() {
-            @Override
-            public void onInsetsChanged(Rect insets) {
-                mTopInset = insets.top;
-            }
-        });
+        bindViews();
 
-        mScrollView = (ObservableScrollView) mRootView.findViewById(R.id.scrollview);
-        mScrollView.setCallbacks(new ObservableScrollView.Callbacks() {
-            @Override
-            public void onScrollChanged() {
-                mScrollY = mScrollView.getScrollY();
-                getActivityCast().onUpButtonFloorChanged(mItemId, ArticleDetailFragment.this);
-                mPhotoContainerView.setTranslationY((int) (mScrollY - mScrollY / PARALLAX_FACTOR));
-                updateStatusBar();
-            }
-        });
+        return mRootView;
+    }
 
-        mPhotoView = (ImageView) mRootView.findViewById(R.id.photo);
-        mPhotoContainerView = mRootView.findViewById(R.id.photo_container);
 
-        mStatusBarColorDrawable = new ColorDrawable(0);
+    private void bindViews() {
+        if (mRootView == null) {
+            return;
+        }
 
-        mRootView.findViewById(R.id.share_fab).setOnClickListener(new View.OnClickListener() {
+
+        activity_detail_coordinatorlayout = (CoordinatorLayout) mRootView.findViewById(R.id.activity_detail_coordinatorlayout);
+        article_detail_share_fab = (FloatingActionButton) mRootView.findViewById(R.id.article_detail_share_fab);
+        article_detail_appbarlayout = (AppBarLayout) mRootView.findViewById(R.id.article_detail_appbarlayout);
+        article_detail_collapsetoolbarlayout = (CollapsingToolbarLayout) mRootView.findViewById(R.id.article_detail_collapsetoolbarlayout);
+        article_detail_image = (ImageView) mRootView.findViewById(R.id.article_detail_image);
+        article_detail_title = (TextView) mRootView.findViewById(R.id.article_detail_title);
+        article_byline = (TextView) mRootView.findViewById(R.id.article_byline);
+        article_detail_description = (TextView) mRootView.findViewById(R.id.article_detail_description);
+        article_detail_toolbar = (Toolbar) mRootView.findViewById(R.id.article_detail_toolbar);
+        article_detail_share_fab = (FloatingActionButton) mRootView.findViewById(R.id.article_detail_share_fab);
+
+
+        //ToDo-title dynamic after fetching content from Loader
+        article_detail_collapsetoolbarlayout.setTitle("Ravindra");
+        article_detail_collapsetoolbarlayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+
+
+
+
+        mRootView.findViewById(R.id.article_detail_share_fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(Intent.createChooser(ShareCompat.IntentBuilder.from(getActivity())
@@ -138,57 +144,14 @@ public class ArticleDetailFragment extends Fragment implements
             }
         });
 
-        bindViews();
-        updateStatusBar();
-        return mRootView;
-    }
 
-    private void updateStatusBar() {
-        int color = 0;
-        if (mPhotoView != null && mTopInset != 0 && mScrollY > 0) {
-            float f = progress(mScrollY,
-                    mStatusBarFullOpacityBottom - mTopInset * 3,
-                    mStatusBarFullOpacityBottom - mTopInset);
-            color = Color.argb((int) (255 * f),
-                    (int) (Color.red(mMutedColor) * 0.9),
-                    (int) (Color.green(mMutedColor) * 0.9),
-                    (int) (Color.blue(mMutedColor) * 0.9));
-        }
-        mStatusBarColorDrawable.setColor(color);
-        mDrawInsetsFrameLayout.setInsetBackground(mStatusBarColorDrawable);
-    }
-
-    static float progress(float v, float min, float max) {
-        return constrain((v - min) / (max - min), 0, 1);
-    }
-
-    static float constrain(float val, float min, float max) {
-        if (val < min) {
-            return min;
-        } else if (val > max) {
-            return max;
-        } else {
-            return val;
-        }
-    }
-
-    private void bindViews() {
-        if (mRootView == null) {
-            return;
-        }
-
-        TextView titleView = (TextView) mRootView.findViewById(R.id.article_title);
-        TextView bylineView = (TextView) mRootView.findViewById(R.id.article_byline);
-        bylineView.setMovementMethod(new LinkMovementMethod());
-        TextView bodyView = (TextView) mRootView.findViewById(R.id.article_body);
-        bodyView.setTypeface(Typeface.createFromAsset(getResources().getAssets(), "Rosario-Regular.ttf"));
 
         if (mCursor != null) {
             mRootView.setAlpha(0);
             mRootView.setVisibility(View.VISIBLE);
             mRootView.animate().alpha(1);
-            titleView.setText(mCursor.getString(ArticleLoader.Query.TITLE));
-            bylineView.setText(Html.fromHtml(
+            article_detail_title.setText(mCursor.getString(ArticleLoader.Query.TITLE));
+            article_byline.setText(Html.fromHtml(
                     DateUtils.getRelativeTimeSpanString(
                             mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
                             System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
@@ -196,7 +159,7 @@ public class ArticleDetailFragment extends Fragment implements
                             + " by <font color='#ffffff'>"
                             + mCursor.getString(ArticleLoader.Query.AUTHOR)
                             + "</font>"));
-            bodyView.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
+            article_detail_description.setText(Html.fromHtml(mCursor.getString(ArticleLoader.Query.BODY)));
             ImageLoaderHelper.getInstance(getActivity()).getImageLoader()
                     .get(mCursor.getString(ArticleLoader.Query.PHOTO_URL), new ImageLoader.ImageListener() {
                         @Override
@@ -204,11 +167,7 @@ public class ArticleDetailFragment extends Fragment implements
                             Bitmap bitmap = imageContainer.getBitmap();
                             if (bitmap != null) {
                                 Palette p = Palette.generate(bitmap, 12);
-                                mMutedColor = p.getDarkMutedColor(0xFF333333);
-                                mPhotoView.setImageBitmap(imageContainer.getBitmap());
-                                mRootView.findViewById(R.id.meta_bar)
-                                        .setBackgroundColor(mMutedColor);
-                                updateStatusBar();
+                                article_detail_image.setImageBitmap(imageContainer.getBitmap());
                             }
                         }
 
@@ -218,10 +177,10 @@ public class ArticleDetailFragment extends Fragment implements
                         }
                     });
         } else {
-            mRootView.setVisibility(View.GONE);
-            titleView.setText("N/A");
-            bylineView.setText("N/A" );
-            bodyView.setText("N/A");
+            //mRootView.setVisibility(View.GONE);
+            article_detail_title.setText("N/A");
+            article_byline.setText("N/A");
+            article_detail_description.setText("N/A");
         }
     }
 
@@ -229,6 +188,7 @@ public class ArticleDetailFragment extends Fragment implements
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
         return ArticleLoader.newInstanceForItemId(getActivity(), mItemId);
     }
+
 
     @Override
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
@@ -255,14 +215,5 @@ public class ArticleDetailFragment extends Fragment implements
         bindViews();
     }
 
-    public int getUpButtonFloor() {
-        if (mPhotoContainerView == null || mPhotoView.getHeight() == 0) {
-            return Integer.MAX_VALUE;
-        }
 
-        // account for parallax
-        return mIsCard
-                ? (int) mPhotoContainerView.getTranslationY() + mPhotoView.getHeight() - mScrollY
-                : mPhotoView.getHeight() - mScrollY;
-    }
 }
--- XYZReader/src/main/java/redgun/example/materialdesign/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/redgun/example/materialdesign/ui/ArticleListActivity.java
@@ -1,16 +1,20 @@
 package redgun.example.materialdesign.ui;
 
-import android.app.LoaderManager;
+
+import android.app.ActivityOptions;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.Loader;
+
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -22,7 +26,6 @@ import android.widget.TextView;
 
 import redgun.example.materialdesign.R;
 import redgun.example.materialdesign.data.ArticleLoader;
-import redgun.example.materialdesign.data.ItemsContract;
 import redgun.example.materialdesign.data.UpdaterService;
 
 /**
@@ -33,7 +36,7 @@ import redgun.example.materialdesign.data.UpdaterService;
  */
 public class ArticleListActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
-
+    public static Context mContext;
     private Toolbar mToolbar;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
@@ -44,18 +47,16 @@ public class ArticleListActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-
+        mContext = this;
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         articleListCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.articleListCoordinatorLayout);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        getLoaderManager().initLoader(0, null, this);
+        getSupportLoaderManager().initLoader(0, null, this);
         if (savedInstanceState == null) {
             refresh();
-
         }
-
 
         mSwipeRefreshLayout.setOnRefreshListener(
                 new SwipeRefreshLayout.OnRefreshListener()
@@ -69,7 +70,6 @@ public class ArticleListActivity extends AppCompatActivity implements
     }
 
     private void refresh() {
-        Intent mIntent = new Intent(this, UpdaterService.class);
         startService(new Intent(this, UpdaterService.class));
     }
 
@@ -164,8 +164,12 @@ public class ArticleListActivity extends AppCompatActivity implements
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            ItemsContract.Items.buildItemUri(getItemId(vh.getAdapterPosition()))));
+                    //Bundle b = ActivityOptions.makeSceneTransitionAnimation(mContext,vh.thumbnailView,vh.thumbnailView.getTransitionName());
+                    Bundle b = new Bundle();
+                    b.putInt("selected_article_id", 1);
+                    Intent intent = new Intent(getApplicationContext(), ArticleDetailActivity.class);
+                    intent.putExtra("bundle", b);
+                    startActivity(intent);
                 }
             });
             return vh;
@@ -193,7 +197,7 @@ public class ArticleListActivity extends AppCompatActivity implements
 
         public ViewHolder(View view) {
             super(view);
-            thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
+            thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.article_image);
             titleView = (TextView) view.findViewById(R.id.article_title);
         }
     }
